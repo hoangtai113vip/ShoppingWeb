5430d5956ce0a96aed1440efed6f29b3cf379fd1
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-12-18 21:26:27 +0700

    Products : Edit Get Action method

--- ShoppingWeb/Areas/Admin/Controllers/ProductsController.cs
+++ ShoppingWeb/Areas/Admin/Controllers/ProductsController.cs
@@ -94,6 +94,25 @@ namespace ShoppingWeb.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+        //GET : Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductsVM);
+        }
+
+
     }
 }
     
