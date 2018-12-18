d0d1271555dd019cda9c94e3bc55baf5aedab6db
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-12-18 20:33:52 +0700

    Products : Create Action Method Part 1

---
+++ ShoppingWeb/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingWeb.Utility
+{
+    public class SD
+    {
+        public const string DefaultProductImage = "default_product.jpg";
+        public const string ImageFolder = @"image\ProductImage";
+    }
+}
