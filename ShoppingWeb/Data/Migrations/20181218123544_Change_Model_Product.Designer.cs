f8e30113cc3202486eabb204d932c0171fc2c7ca
hoangvantai1131997 <hoangtai113vip@gmail.com>
2018-12-18 19:36:27 +0700

    Migration change products

--- ShoppingWeb/Data/Migrations/20181218123544_Change_Model_Product.Designer.cs
+++ ShoppingWeb/Data/Migrations/20181218123544_Change_Model_Product.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingWeb.Data;
 
 namespace ShoppingWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181218123544_Change_Model_Product")]
+    partial class Change_Model_Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShoppingWeb/Data/Migrations/20181218123544_Change_Model_Product.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoppingWeb.Data.Migrations
+{
+    public partial class Change_Model_Product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Available",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductTypeId",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShadeColor",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpecialTagsID",
+                table: "Products",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SpecialTagsID",
+                table: "Products",
+                column: "SpecialTagsID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_SpecialTags_SpecialTagsID",
+                table: "Products",
+                column: "SpecialTagsID",
+                principalTable: "SpecialTags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_SpecialTags_SpecialTagsID",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SpecialTagsID",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Available",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ShadeColor",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialTagsID",
+                table: "Products");
+        }
+    }
+}
